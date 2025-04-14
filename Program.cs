d0fd1c4a2ac58eb2ddf72b592b72e8c58239add2
GarretCarmouche <carmouchegarret@gmail.com>
2025-04-14 10:31:33 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    private static string watchDirectory = "/watchDirectory";
+    public static void Main(string[] args)
+    {
+        int checkDelay = int.Parse("" + Environment.GetEnvironmentVariable("CHECKFREQUENCY")) * 1000;
+        while (true) {
+            Thread.Sleep(checkDelay);
+            RunCheck();
+        }
+    }
+
+    private static void RunCheck()
+    {
+        Console.WriteLine("Running scan");
+        foreach(string source in Directory.GetDirectories(watchDirectory))
+        {
+            Console.WriteLine("Source: " + source);
+            foreach(string video in Directory.GetDirectories(source))
+            {
+                Console.WriteLine("Video: " + video);
+
+                if (File.Exists(video + "/background.jpg"))
+                    continue;
+                
+                foreach(string file in Directory.GetFiles(video))
+                {
+                    Console.WriteLine("File: " + file);
+                    if (file.EndsWith(".jpg"))
+                    {
+                        File.Copy(file, file.Substring(0, file.Length - Path.GetFileName(file).Length) + "/background.jpg", true);
+                    }
+                }
+            }
+        }
+    }
+}
